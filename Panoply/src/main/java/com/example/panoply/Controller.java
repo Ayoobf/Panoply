43289ddfd1c8dc47f7b01a3763d8dd5d82a1c9f0
Ayoobf <rflooivl@gmail.com>
2023-11-03 20:11:50 -0400

    Implemented draggable behavior on login screen and therefore subsequent screens

--- Panoply/src/main/java/com/example/panoply/Controller.java
+++ Panoply/src/main/java/com/example/panoply/Controller.java
@@ -2,21 +2,77 @@ package com.example.panoply;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class Controller {
+
     @FXML
     private Button btExit;
 
     @FXML
+    private Button btLogin;
+
+    @FXML
+    private Pane imageArea;
+
+    @FXML
+    private Hyperlink lCreateAccount;
+
+    @FXML
+    private Hyperlink lReset;
+
+    @FXML
+    private VBox loginArea;
+
+    @FXML
+    private TextField tfPassword;
+
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
     void btExit(ActionEvent event) {
-        btExit.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Platform.exit();
-            }
-        });
+        Platform.exit();
     }
+
+    @FXML
+    void btLogin(ActionEvent event) {
+
+        if (!tfUsername.getText().isEmpty() && !tfPassword.getText().isEmpty()) {
+            System.out.println(tfUsername.getText().trim() + " " + tfPassword.getText().trim());
+
+        }
+
+    }
+
+    @FXML
+    void lCreateAccount(ActionEvent event) {
+        System.out.println("create");
+    }
+
+    @FXML
+    void lReset(ActionEvent event) {
+        System.out.println("reset");
+    }
+
+    @FXML
+    void tfPassword(ActionEvent event) {
+
+        String password = tfPassword.getText();
+
+    }
+
+    @FXML
+    void tfUsername(ActionEvent event) {
+
+        String username = tfUsername.getText();
+
+    }
+
+
 }
--- Panoply/src/main/java/com/example/panoply/HelloApplication.java
+++ Panoply/src/main/java/com/example/panoply/HelloApplication.java
@@ -1,24 +1,57 @@
 package com.example.panoply;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    // Helper method to help me move the window
+    private static void mouseDraggedToMoveWindow(Stage stage, Scene scene, Delta dragDelta) {
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                stage.setX(mouseEvent.getScreenX() + dragDelta.x);
+                stage.setY(mouseEvent.getScreenY() + dragDelta.y);
+
+            }
+        });
+    }
+
+    // Helper method to help me move the window
+    private static void mousePressedForDrag(Stage stage, Scene scene, Delta dragDelta) {
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                dragDelta.x = stage.getX() - mouseEvent.getScreenX();
+                dragDelta.y = stage.getY() - mouseEvent.getScreenY();
+
+            }
+        });
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
 
-//        // Implement my own buttons
-//        stage.initStyle(StageStyle.UNDECORATED);
+        // Implement my own buttons
+        stage.initStyle(StageStyle.UNDECORATED);
 
+        // Implement my own Drag behavior
+        final Delta dragDelta = new Delta();
+        mousePressedForDrag(stage, scene, dragDelta);
+        mouseDraggedToMoveWindow(stage, scene, dragDelta);
+
+        // default garbage
         stage.setTitle("Panoply");
-        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.setScene(scene);
         stage.setResizable(false);
-
         stage.show();
     }
 

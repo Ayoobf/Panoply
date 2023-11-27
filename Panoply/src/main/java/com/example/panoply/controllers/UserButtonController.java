5e52d44d2efa4132c0e9693b1cf76d5297862163
Ayoob Florival <rflooivl@gmail.com>
2023-11-27 15:23:07 -0500

    Added TODO

--- Panoply/src/main/java/com/example/panoply/controllers/UserButtonController.java
+++ Panoply/src/main/java/com/example/panoply/controllers/UserButtonController.java
@@ -7,10 +7,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import User.java;
+
 
 public class UserButtonController extends DefaultController implements Initializable {
     public Pane userBtPane;
     public Button btUser;
+    User foo = new User();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {

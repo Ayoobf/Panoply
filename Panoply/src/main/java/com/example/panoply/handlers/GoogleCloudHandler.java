8384d7174256d66d9ece09026c45caa4c739a889
Ayoobf <rflooivl@gmail.com>
2023-12-01 20:30:24 -0500

    Implemented DownloadFile

--- Panoply/src/main/java/com/example/panoply/handlers/GoogleCloudHandler.java
+++ Panoply/src/main/java/com/example/panoply/handlers/GoogleCloudHandler.java
@@ -1,9 +1,14 @@
 package com.example.panoply.handlers;
 
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GoogleCloudHandler {
     final static String PROJECT_ID = "ayoob-florival-capstone";
@@ -19,5 +24,25 @@ public class GoogleCloudHandler {
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
 
         storage.create(blobInfo, new byte[0]); // Empty byte array creates a zero-byte object
+    }
+
+    public void downloadFile(String teamName, String fileName) {
+        // The ID of your GCS object
+        teamName = teamName + "/";
+
+        final String initPath = "C:\\Panoply";
+        try {
+            Files.createDirectories(Paths.get(initPath + "\\" + teamName));
+            // The path to which the file should be downloaded
+            String destFilePath = initPath + "\\" + teamName + fileName;
+
+
+            BlobId blobId = BlobId.of(BUCKET_NAME, teamName + fileName);
+            Blob blob = storage.get(blobId);
+
+            blob.downloadTo(Paths.get(destFilePath));
+
+        } catch (IOException ignored) {
+        }
     }
 }

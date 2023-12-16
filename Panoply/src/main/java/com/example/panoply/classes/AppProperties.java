5a1d019083b8632c864faec8a9cd797457f36f04
Ayoobf <rflooivl@gmail.com>
2023-12-16 15:34:08 -0500

    Added properties File use

---
+++ Panoply/src/main/java/com/example/panoply/classes/AppProperties.java
@@ -0,0 +1,54 @@
+package com.example.panoply.classes;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AppProperties {
+
+	private static final AppProperties config_file = new AppProperties();
+	// FILENAME = Path to properties-file
+	// Store and protect it where ever you want
+	private final String FILENAME = "app.properties";
+	private Properties prop = new Properties();
+	private String msg = "";
+
+	private AppProperties() {
+		InputStream input = null;
+
+		try {
+			input = new FileInputStream(FILENAME);
+
+			// Load a properties
+			prop.load(input);
+		} catch (IOException ex) {
+			msg = "Can't find/open property file";
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	// == Singleton design pattern == //
+	// Where ever you call this methode in application
+	// you always get the same and only instance (config_file)
+	public static AppProperties getInstance() {
+		return config_file;
+	}
+
+	public String getProperty(String key) {
+		return prop.getProperty(key);
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+}

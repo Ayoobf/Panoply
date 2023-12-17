624f99428b71130d2b02a4ac7929174a923d333c
Ayoobf <rflooivl@gmail.com>
2023-12-17 14:07:13 -0500

    Added Docs for Document.java

--- Panoply/src/main/java/com/example/panoply/classes/Document.java
+++ Panoply/src/main/java/com/example/panoply/classes/Document.java
@@ -5,10 +5,12 @@ import com.example.panoply.handlers.MongoDBHandler;
 
 import org.bson.BsonDateTime;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
+/**
+ * @author Ayoob Florival
+ */
 public class Document {
 	private String documentId;
 	private String documentName;
@@ -19,16 +21,34 @@ public class Document {
 	private Object lastEditor;
 	private boolean isCheckOut;
 
+	/**
+	 * Handle cases where a document would interact with Google and MongoDB
+	 */
 	public Document() {
 	}
 
+	/**
+	 * Constructor for when document path is provided
+	 *
+	 * @param documentPath path to file in which needs to be uploaded
+	 */
 	public Document(String documentPath) {
 		this.documentPath = documentPath;
 		this.documentName = Paths.get(documentPath).getFileName().toString();
 
 	}
 
-
+	/**
+	 * Uploads File
+	 * <p>
+	 * Interacts with MongoDB and Google to upload a file from local to the cloud
+	 * </p>
+	 *
+	 * @param documentPath        path to where document is on the local machine
+	 * @param currentUserTeamName team name of current user
+	 * @param userName            username/email of the current user
+	 * @throws IOException ioexception from file handling
+	 */
 	public void uploadDocument(String documentPath, String currentUserTeamName, String userName) throws IOException {
 
 		String fileName = Paths.get(documentPath).getFileName().toString();
@@ -37,10 +57,20 @@ public class Document {
 
 	}
 
+	/**
+	 * getter for the current document's path
+	 *
+	 * @return document path
+	 */
 	public String getDocumentPath() {
 		return documentPath;
 	}
 
+	/**
+	 * setter for upload date. upload date usually is always time of upload
+	 *
+	 * @param uploadDate date snapshot to set document date
+	 */
 	public void setUploadDate(BsonDateTime uploadDate) {
 		this.uploadDate = uploadDate;
 	}

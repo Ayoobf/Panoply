7253d48ca29dfe80897345fb34d1736f66ee50b7
Ayoobf <rflooivl@gmail.com>
2023-12-17 13:59:10 -0500

    Added Docs for UserHolder.java

--- Panoply/src/main/java/com/example/panoply/classes/UserHolder.java
+++ Panoply/src/main/java/com/example/panoply/classes/UserHolder.java
@@ -1,5 +1,8 @@
 package com.example.panoply.classes;
 
+/**
+ * @author Ayoob Florival
+ */
 public final class UserHolder {
 
 	private final static UserHolder INSTANCE = new UserHolder();
@@ -8,14 +11,29 @@ public final class UserHolder {
 	private UserHolder() {
 	}
 
+	/**
+	 * Instance getter
+	 *
+	 * @return Instance of userholder
+	 */
 	public static UserHolder getINSTANCE() {
 		return INSTANCE;
 	}
 
+	/**
+	 * getter for the instance user
+	 *
+	 * @return instance user
+	 */
 	public User getUser() {
 		return user;
 	}
 
+	/**
+	 * setter for the instance user
+	 *
+	 * @param user user object to which to hold in UserHolder
+	 */
 	public void setUser(User user) {
 		this.user = user;
 	}

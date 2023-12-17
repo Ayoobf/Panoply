0e125c3b99e511eab0eb7415c908b63fb9c629fb
Ayoobf <rflooivl@gmail.com>
2023-12-17 13:17:12 -0500

    Added Docs for AppProperties

--- Panoply/src/main/java/com/example/panoply/classes/AppProperties.java
+++ Panoply/src/main/java/com/example/panoply/classes/AppProperties.java
@@ -5,25 +5,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+/**
+ * @author 68Doom68 (stackoverflow)
+ */
 public class AppProperties {
 
 	private static final AppProperties config_file = new AppProperties();
-	// FILENAME = Path to properties-file
-	// Store and protect it where ever you want
-	private final String FILENAME = "app.properties";
-	private Properties prop = new Properties();
-	private String msg = "";
+	private final Properties prop = new Properties();
 
+	/**
+	 * Singleton constructor for returning a properties file's contents
+	 */
 	private AppProperties() {
 		InputStream input = null;
 
 		try {
+			// Store and protect it where ever you want
+			String FILENAME = "app.properties";
 			input = new FileInputStream(FILENAME);
 
 			// Load a properties
 			prop.load(input);
 		} catch (IOException ex) {
-			msg = "Can't find/open property file";
 			ex.printStackTrace();
 		} finally {
 			if (input != null) {
@@ -37,18 +40,27 @@ public class AppProperties {
 		}
 	}
 
-	// == Singleton design pattern == //
-	// Where ever you call this methode in application
-	// you always get the same and only instance (config_file)
+	/**
+	 * Singleton design pattern
+	 * <p>
+	 * Where ever you call this method, you will always get the same and oly one instance of config_file
+	 * </p>
+	 *
+	 * @return config file in our instance
+	 * @since 1.0
+	 */
 	public static AppProperties getInstance() {
 		return config_file;
 	}
 
+	/**
+	 * getter for property file
+	 *
+	 * @param key The key in which the caller is looking for
+	 * @return value of the key specified. If no key is returned
+	 */
 	public String getProperty(String key) {
 		return prop.getProperty(key);
 	}
 
-	public String getMsg() {
-		return msg;
-	}
 }
